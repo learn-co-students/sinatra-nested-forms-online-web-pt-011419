24595ed4e8e368b010344162d8ef6722a9f55957
blackwell <blackwellsmithiv@gmail.com>
2019-04-20 18:08:23 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,9 +1,20 @@
 require './environment'
-
+require 'pry'
 module FormsLab
   class App < Sinatra::Base
-
-    # code other routes/actions here
-
+    get '/' do
+      "Welcome to the Nested Forms Lab!"
+    end
+    
+    get '/new' do
+      erb :'pirates/new'
+    end
+    
+    post '/pirates' do
+     # binding.pry
+      erb :'pirates/show'
+    #  binding.pry
   end
+end
+
 end
