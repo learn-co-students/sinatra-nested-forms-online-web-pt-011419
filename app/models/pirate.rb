040139cbce1c3bdf8eebd2fc897bb915e965edcc
Andrea Lovewell <andy.lovewell@gmail.com>
2019-05-01 05:17:21 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,18 @@
 class Pirate
+
+  attr_accessor :name, :weight, :height
+
+  PIRATES =[]
+
+  def intialize (params)
+    @name = params[:name]
+    @weight = params[:weight]
+    @height = height [:height]
+    PIRATES << self
+  end
+
+  def self.all
+    PIRATES
+  end
+
 end
