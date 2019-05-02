59a910d1d6e3f030ece7d9df948192416a3b3cd3
imaglazer <avigayilglazer@gmail.com>
2019-05-01 22:34:07 -0400

    Done.

--- app.rb
+++ app.rb
@@ -4,6 +4,11 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/pirates' do
+     @pirates = Pirate.all
+     
+     erb :'pirates/index'
+    end
 
   end
 end
