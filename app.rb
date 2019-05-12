fa70ad9d106d58fb9b841d7b52b938c369d541e5
imaglazer <avigayilglazer@gmail.com>
2019-05-12 09:51:57 +0300

    Done.

--- app.rb
+++ app.rb
@@ -4,11 +4,30 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
-    get '/pirates' do
-     @pirates = Pirate.all
-     
-     erb :'pirates/index'
+    get '/' do
+      erb :root
     end
 
+    get '/new' do
+     erb :'pirates/new'
+    end
+  
+    # get '/pirates/:id' do
+    #   @pirate = Pirate.find(params[:id])
+
+    #   erb :'pirates/show'
+    # end
+    
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
+      end
+      @ships = Ship.all
+
+      erb :'pirates/show'
+    end
+ 
   end
 end
--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,3 +1,18 @@
-class Pirate < ActiveRecord::Base
-    has_many :ships
+class Pirate 
+    # has_many :ships
+
+    attr_accessor :name, :weight, :height
+    PIRATES = []
+
+    def initialize(args)
+        @name = args[:name]
+        @weight = args[:weight]
+        @height = args[:height]
+        PIRATES << self
+    end
+
+    def self.all
+        PIRATES
+    end
+
 end
